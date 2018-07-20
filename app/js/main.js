7792b0fc87a61554e0efdc8ca869be2383a28f91
TuMaN1122 <tuman-1122@yandex.ru>
2018-07-20 12:46:03 +0300

    Сборка обновлена до версии 1.6

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+
+// Для кроссбраузерности svg спрайтов подключаем её и инициализируем библиотеку  svg4everybody.
+  svg4everybody({}); 
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp       		= require('gulp'), // Подключаем Gulp
 	less 			= require('gulp-less'); //Подключаем Less пакет
 	browserSync  	= require('browser-sync'), // Подключаем Browser Sync
-	notify			= require('gulp-notify'),	//Сообщения об ошибках и предупреждениях об ошибках
-	plumber			= require('gulp-plumber'),	//Предотвращает падение(зависание) GULP при ошибках
+	notify			= require('gulp-notify'),	//Сообщения об ошибках
+	plumber			= require('gulp-plumber'),	//Предотвращает остановку GULP при ошибках
 	concat       	= require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
 	uglify       	= require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
 	cssnano      	= require('gulp-cssnano'), // Подключаем пакет для минификации CSS
@@ -11,10 +11,16 @@ var gulp       		= require('gulp'), // Подключаем Gulp
 	imagemin     	= require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
 	pngquant     	= require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
 	cache        	= require('gulp-cache'), // Подключаем библиотеку кеширования
-	autoprefixer 	= require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
+	autoprefixer 	= require('gulp-autoprefixer'),// Подключаем библиотеку для автоматического добавления префиксов
+	svgSprite 		= require('gulp-svg-sprite'), //Создание спрайта
+	svgmin 			= require('gulp-svgmin'), //Минификация SVG
+	cheerio 		= require('gulp-cheerio'), //Удаление лишних атрибутов из svg
+	replace 		= require('gulp-replace'); //Решаем баг когда символ ‘>’ преобразуется в кодировку '&gt;'. 
+
+var appDir 			= 'app/';
 
 gulp.task('less', function(){ // Создаем таск less
-	return gulp.src('app/less/*.less') // Берем источник
+	return gulp.src(['app/less/main.less', 'app/less/libs.less']) // Берем источник
 		
 		.pipe(plumber({ 
 			errorHandler: notify.onError(function(err){
@@ -23,7 +29,7 @@ gulp.task('less', function(){ // Создаем таск less
 					message: err.massage
 				}
 			})
-		})) //Предотвращает падение(зависание) GULP при ошибках
+		})) //Предотвращает остановку GULP при ошибках
 
 		.pipe(less()) // Преобразуем less в CSS посредством gulp-less
 		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
@@ -43,7 +49,8 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 gulp.task('scripts', function() {
 	return gulp.src([ // Берем все необходимые библиотеки
 		'app/libs/jquery/jquery-3.3.1.min.js', // Берем jQuery
-		'app/libs/pagescroll2id/jquery.malihu.PageScroll2id.js' // Берем плавная пракрутка
+		'app/libs/pagescroll2id/jquery.malihu.PageScroll2id.js',  // Берем плавная пракрутка
+		'app/libs/svg4everybody/svg4everybody.js'
 		])
 		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
 		.pipe(uglify()) // Сжимаем JS файл
@@ -68,22 +75,69 @@ gulp.task('clean', function() {
 });
 
 gulp.task('img', function() {
-	return gulp.src('app/img/**/*') // Берем все изображения из app
-		.pipe(cache(imagemin({ // С кешированием
+	return gulp.src('app/img/**/*.{png,jpg,svg}') // Берем все изображения из app
+		// .pipe(cache(imagemin({ // С кешированием
 		// .pipe(imagemin({ // Сжимаем изображения без кеширования
-			interlaced: true,
-			progressive: true,
-			svgoPlugins: [{removeViewBox: false}],
-			use: [pngquant()]
-		}))/**/)
+			.pipe(cache(imagemin([
+			    imagemin.gifsicle({interlaced: true}),
+			    imagemin.jpegtran({progressive: true}),
+			    imagemin.optipng({optimizationLevel: 3}),
+			    imagemin.svgo({
+			        plugins: [
+			            {removeViewBox: true},
+			            {cleanupIDs: false}
+			        ]
+	    		})
+			])))
 		.pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+});
+
+// --------------------------------------------Создание SVG спрайтов
+
+
+gulp.task('svg', function () {
+	return gulp.src(appDir + 'img/svg/icons/*svg')
+	// minify svg
+	.pipe(svgmin({
+		js2svg: {
+			pretty: true
+		}
+	}))
+	// remove all fill and style declarations in out shapes
+	.pipe(cheerio({
+		run: function ($) {
+			$('[fill]').removeAttr('fill');
+			$('[stroke]').removeAttr('stroke');
+			$('[style]').removeAttr('style');
+		},
+		parserOptions: {xmlMode: true}
+	}))
+	// cheerio plugin create unnecessary string '&gt;', so replace it.
+	.pipe(replace('&gt;', '>'))
+	// build svg sprite
+	.pipe(svgSprite({
+		mode: {
+			symbol: {
+				sprite: "../sprite.svg",
+				render: {
+					less: {
+						dest:'../_sprite.less',
+						template: appDir + "less/iconsvg/_sprite_template.less"
+					}
+				},
+				example: true
+			}
+		}
+	}))
+	.pipe(gulp.dest(appDir + 'img/svg/sprite/'));
 });
 
 gulp.task('build', ['clean', 'img', 'less', 'scripts'], function() {
 
 	var buildCss = gulp.src([ // Переносим библиотеки в продакшен
-		'app/css/main.css',
-		'app/css/libs.min.css'
+		// 'app/css/main.css',
+		// 'app/css/libs.min.css'
+		'app/css/*.min.css'
 		])
 	.pipe(gulp.dest('dist/css'))
 
@@ -93,7 +147,7 @@ gulp.task('build', ['clean', 'img', 'less', 'scripts'], function() {
 	var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
 	.pipe(gulp.dest('dist/js'))
 
-	var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен.
+	var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
 	.pipe(gulp.dest('dist'));
 
 });
